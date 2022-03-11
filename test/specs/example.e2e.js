975549d0fcdc78fed706d4fd6c37d356a0b55ca0
Sonia Pugach <soniapugach@Sonias-MacBook-Air-2.local>
2022-03-11 14:44:09 -0800

    changes order of test

--- test/specs/example.e2e.js
+++ test/specs/example.e2e.js
@@ -1,3 +1,17 @@
+describe('My Login application', () => {
+    it('should login with valid credentials', async () => {
+        await browser.url(`https://bstackdemo.com/signin`);
+
+        await $('#username').setValue('demouser');
+        await $('#password').setValue('testingisfun99');
+        await $('button[type="submit"]').click();
+
+        await expect($('#flash')).toBeExisting();
+        await expect($('#flash')).toHaveTextContaining(
+            'You logged into a secure area!');
+    });
+});
+
 // describe("Google's Search Functionality", () => {
 //     it("can find search results", () => {
 //       browser.url("https://www.google.com/");
@@ -17,19 +31,3 @@
 //       );
 //     });
 //   });
-  
-
-describe('My Login application', () => {
-    it('should login with valid credentials', async () => {
-        await browser.url(`https://bstackdemo.com/signin`);
-
-        await $('#username').setValue('demouser');
-        await $('#password').setValue('testingisfun99');
-        await $('button[type="submit"]').click();
-
-        await expect($('#flash')).toBeExisting();
-        await expect($('#flash')).toHaveTextContaining(
-            'You logged into a secure area!');
-    });
-});
-
