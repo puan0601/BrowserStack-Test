6e757827b6a22ad24771b380943ea3ecffffddc0
Sonia Pugach <soniapugach@Sonias-MacBook-Air-2.local>
2022-03-11 14:49:56 -0800

    temporarily removes google test

--- test/specs/example.e2e.js
+++ test/specs/example.e2e.js
@@ -12,22 +12,3 @@ describe('My Login application', () => {
     });
 });
 
-// describe("Google's Search Functionality", () => {
-//     it("can find search results", () => {
-//       browser.url("https://www.google.com/");
-//       const inputForm = $("//input[@name='q']");
-//       inputForm.setValue(["BrowserStack", "Enter"]); // this helps in majority desktops
-  
-//       try {
-//         browser.waitUntil(() => browser.getTitle().match(/BrowserStack/i));
-//       } catch (e) {
-//         browser.elementSubmit(inputForm.elementId); // this helps in remaining cases, i.e. iPhone
-//       }
-  
-//       browser.waitUntil(
-//         () => browser.getTitle().match(/BrowserStack/i),
-//         5000,
-//         "Title didn't match with BrowserStack"
-//       );
-//     });
-//   });
