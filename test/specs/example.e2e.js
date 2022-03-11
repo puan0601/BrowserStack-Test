8cb7b43e64c88e7e5081dfa8f3b51da1bb74383a
Sonia Pugach <soniapugach@Sonias-MacBook-Air-2.local>
2022-03-11 14:41:11 -0800

    Adds bstackdemo login test

--- test/specs/example.e2e.js
+++ test/specs/example.e2e.js
@@ -1,35 +1,35 @@
-describe("Google's Search Functionality", () => {
-    it("can find search results", () => {
-      browser.url("https://www.google.com/");
-      const inputForm = $("//input[@name='q']");
-      inputForm.setValue(["BrowserStack", "Enter"]); // this helps in majority desktops
+// describe("Google's Search Functionality", () => {
+//     it("can find search results", () => {
+//       browser.url("https://www.google.com/");
+//       const inputForm = $("//input[@name='q']");
+//       inputForm.setValue(["BrowserStack", "Enter"]); // this helps in majority desktops
   
-      try {
-        browser.waitUntil(() => browser.getTitle().match(/BrowserStack/i));
-      } catch (e) {
-        browser.elementSubmit(inputForm.elementId); // this helps in remaining cases, i.e. iPhone
-      }
+//       try {
+//         browser.waitUntil(() => browser.getTitle().match(/BrowserStack/i));
+//       } catch (e) {
+//         browser.elementSubmit(inputForm.elementId); // this helps in remaining cases, i.e. iPhone
+//       }
   
-      browser.waitUntil(
-        () => browser.getTitle().match(/BrowserStack/i),
-        5000,
-        "Title didn't match with BrowserStack"
-      );
-    });
-  });
-  
-
-// describe('My Login application', () => {
-//     it('should login with valid credentials', async () => {
-//         await browser.url(`https://bstackdemo.com/`);
-
-//         await $('#username').setValue('tomsmith');
-//         await $('#password').setValue('SuperSecretPassword!');
-//         await $('button[type="submit"]').click();
-
-//         await expect($('#flash')).toBeExisting();
-//         await expect($('#flash')).toHaveTextContaining(
-//             'You logged into a secure area!');
+//       browser.waitUntil(
+//         () => browser.getTitle().match(/BrowserStack/i),
+//         5000,
+//         "Title didn't match with BrowserStack"
+//       );
 //     });
-// });
+//   });
+  
+
+describe('My Login application', () => {
+    it('should login with valid credentials', async () => {
+        await browser.url(`https://bstackdemo.com/signin`);
+
+        await $('#username').setValue('demouser');
+        await $('#password').setValue('testingisfun99');
+        await $('button[type="submit"]').click();
+
+        await expect($('#flash')).toBeExisting();
+        await expect($('#flash')).toHaveTextContaining(
+            'You logged into a secure area!');
+    });
+});
 
