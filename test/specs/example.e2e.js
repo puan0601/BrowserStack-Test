8bba885897aaf4d84a0d8802bf984b476e7e2064
Sonia Pugach <soniapugach@Sonias-MacBook-Air-2.local>
2022-03-11 18:04:12 -0800

    Adds sign in link test on homepage

--- test/specs/example.e2e.js
+++ test/specs/example.e2e.js
@@ -4,25 +4,51 @@ describe('BStackdemo Home', () => {
     it('Should load with the proper title', async () => {
         await browser.url("https://bstackdemo.com/");
         await expect(browser).toHaveTitle('StackDemo');
-        // await $('#signin').click();
-
-        // await $('#username').setValue(['demouser', 'enter']);
-        // await $('#password').setValue(['testingisfun99', 'enter']);
-        // await $('button[type="submit"]').click();
-
-        // await expect($('.username')).toBeExisting();
-        // await expect($('.username')).toHaveTextContaining(
-        //     'demouser');
     });
-    it('Should display product item', async () => {
+
+    it('Should display a product item', async () => {
         await browser.url("https://bstackdemo.com/");
         const item = $('.shelf-item');
         await expect(item).toExist();
     });
+
+    it('Should display multiple product items', async () => {
+        await browser.url("https://bstackdemo.com/");
+        const items = $$('.shelf-item');
+        await expect(items).length < 1;
+    });
+
     it('Should have a Sign In link', async () => {
         await browser.url("https://bstackdemo.com/");
         const signin = $('#signin');
         await expect(signin).toExist();
     })
+
+    it('Sign In link should bring you to the sign in page', async () => {
+        await browser.url("https://bstackdemo.com/");
+        const signin = $('#signin');
+        await signin.click();
+        await expect(browser).toHaveUrl('https://bstackdemo.com/signin');
+    })
 });
+
+describe('Sign In', () => {
+    it('Should have a username input', async () => {
+        await browser.url("https://bstackdemo.com/signin");
+        const usernameInput = $('#username');
+        await expect (usernameInput).toExist();
+    });
+
+    it('Should have a password input', async () => {
+        await browser.url("https://bstackdemo.com/signin");
+        const passwordInput = $('#password');
+        await expect (passwordInput).toExist();
+    });
+
+    it('Should have a login button', async () => {
+        await browser.url("https://bstackdemo.com/signin");
+        const button = $('#login-btn');
+        await expect (button).toExist();
+    });
+})
 
