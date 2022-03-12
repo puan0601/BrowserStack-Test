87f591605ca6b74c571962b860607a22dcfcade7
Sonia Pugach <soniapugach@Sonias-MacBook-Air-2.local>
2022-03-11 17:30:13 -0800

    Adds product item and sign in link tests

--- test/specs/example.e2e.js
+++ test/specs/example.e2e.js
@@ -1,7 +1,9 @@
-describe('My Login application', () => {
-    it('Homepage should load properly', async () => {
-        browser.url("https://bstackdemo.com/");
-        await expect(browser).toHaveTitleContaining('Stack');
+const { default: BrowserstackService } = require("@wdio/browserstack-service");
+
+describe('BStackdemo Home', () => {
+    it('Should load with the proper title', async () => {
+        await browser.url("https://bstackdemo.com/");
+        await expect(browser).toHaveTitle('StackDemo');
         // await $('#signin').click();
 
         // await $('#username').setValue(['demouser', 'enter']);
@@ -12,5 +14,15 @@ describe('My Login application', () => {
         // await expect($('.username')).toHaveTextContaining(
         //     'demouser');
     });
+    it('Should display product item', async () => {
+        await browser.url("https://bstackdemo.com/");
+        const item = $('.shelf-item');
+        await expect(item).toExist();
+    });
+    it('Should have a Sign In link', async () => {
+        await browser.url("https://bstackdemo.com/");
+        const signin = $('#signin');
+        await expect(signin).toExist();
+    })
 });
 
